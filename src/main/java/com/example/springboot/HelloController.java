d536480b61c416454f81cfdd67c7882de55af249
ybhagwan <99209398+ybhagwan@users.noreply.github.com>
2022-02-16 16:32:46 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring ybhagwan VMware + Tanzu!";
+		return "Greetings from Spring updated VMware + Tanzu!";
 	}
 
 }
